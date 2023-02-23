f2fbc54a825f72a8feaed989041457a5e72b94fe
yegoncollins <collinsyegon816@gmail.com>
2023-02-23 11:30:34 +0300

    first

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='text-white'>
+      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+          <p className='text-[#00fffb] font-bold p-2'>GROW WITH DATA ANALYTICS</p>
+          <h1>Grow with data.</h1>
+          <div>
+            <p>Fast, Flexible financing for</p>
+          </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Hero
